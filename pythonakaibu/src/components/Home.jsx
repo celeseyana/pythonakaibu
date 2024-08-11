6d3c611067cb776f14c074a9c367bd0c5515f129
Ong Zi Yang <celeskyanpu@gmail.com>
2024-08-11 12:58:33 +0800

    file swapped
    yurr

--- pythonakaibu/src/components/Home.jsx
+++ pythonakaibu/src/components/Home.jsx
@@ -1,47 +1,9 @@
-import './Home.css';
-import Yurubg from './Yurubg';
 import 'beercss'
 
-
-export default function Home() {
-    return (
-        <>
-            <div>
-                <Yurubg />
-            </div>
-
-            <div className="login-container absolute">
-                <div className='login-logo-welcome'>
-                    <img className='center' src='./src/assets/python.svg'></img>
-                    <span>A conquest-inspired interactive Python learning game.</span>
-                </div>
-                <div className='login-fields white'>
-                    
-                    <span>Login</span>
-                    <span>Please enter your credentials.</span>
-
-                    <div className="login-username field border label">
-                        <input />
-                        <label>Username</label>
-                    </div>
-
-                    <div className="login-password field border label">
-                        <input />
-                        <label>Password</label>
-                    </div>
-
-                    <div className='login-buttons center'>
-                        <button className='register-button'>Register</button>
-                        <button className='login-button'>Login</button>
-                    </div>
-                    
-                </div>
-            </div>
-        </>
-    )
+function Home() {
+  return (
+    <div>home test</div>
+  )
 }
 
-
-// REMINDER TO MOVE THIS TO LOGIN INSTEAD OF HOME
-// REMINDER TO MOVE THIS TO LOGIN INSTEAD OF HOME
-// REMINDER TO MOVE THIS TO LOGIN INSTEAD OF HOME
+export default Home
--- pythonakaibu/src/components/Login.jsx
+++ pythonakaibu/src/components/Login.jsx
@@ -1,12 +1,44 @@
 import './Login.css'
-import "beercss";
-import "material-dynamic-colors";
+import Yurubg from './Yurubg';
+import 'beercss'
 
 export default function Login() {
   return (
-    <div>
+    <>
+      <div>
+        <Yurubg />
+      </div>
 
-    </div>
+      <div className="login-container absolute">
+        
+        <div className='login-logo-welcome'>
+          <img className='center' src='./src/assets/python.svg'></img>
+          <span>A conquest-inspired interactive Python learning game.</span>
+        </div>
+
+        <div className='login-fields white'>
+
+          <span>Login</span>
+          <span>Please enter your credentials.</span>
+
+          <div className="login-username field border label">
+            <input type='text' name='text' />
+            <label>Username</label>
+          </div>
+
+          <div className="login-password field border label">
+            <input type='password' name='password' />
+            <label>Password</label>
+          </div>
+
+          <div className='login-buttons center'>
+            <button className='register-button green3 black-text'>Register</button>
+            <button className='login-button green3 black-text' type='submit'>Login</button>
+          </div>
+
+        </div>
+      </div>
+    </>
   )
 }
 
