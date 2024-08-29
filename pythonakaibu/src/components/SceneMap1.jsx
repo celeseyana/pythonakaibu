1c51a6408966ff39952492592c0d3cd54254d80f
Ong Zi Yang <celeskyanpu@gmail.com>
2024-08-29 11:18:43 +0800

    map1 test added
    added torii test

--- pythonakaibu/src/components/SceneMap1.jsx
+++ pythonakaibu/src/components/SceneMap1.jsx
@@ -1,5 +1,6 @@
 import { Cylinder, OrbitControls } from '@react-three/drei'; 
 import { CylinderCollider, RigidBody } from '@react-three/rapier';
+import { Torii } from './Torii';
 
 const SceneMap1 = () => {
     return (
@@ -12,6 +13,9 @@ const SceneMap1 = () => {
                 castShadow
                 color={"#9e69da"}
             />
+
+            <Torii scale={[16, 16, 16]} position={[0, 0, -22]} rotation-y={1.25 * Math.PI} />
+
             <RigidBody colliders={false} type='fixed' position-y={-0.5}>
                 <CylinderCollider args={[1/2, 5]} />
                 <Cylinder scale={[5, 1, 5]} receiveShadow>
