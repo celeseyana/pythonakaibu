ef3c1c69493ca8cf868f3e69c4c822101da7ca70
Ong Zi Yang <celeskyanpu@gmail.com>
2024-10-16 13:33:41 +0800

    added p1 name from db

--- pythonakaibu/src/components/GameUI.jsx
+++ pythonakaibu/src/components/GameUI.jsx
@@ -4,12 +4,25 @@ import React from 'react'
 import HealthBar from './HealthBar';
 import HealthBar2 from './HealthBar2';
 import PowerupUI from './PowerupUI';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 export default function GameUI({ turnCount, username2 }) {
+    const [name, setName] = useState('');
+
+    axios.defaults.withCredentials = true;
+
+    useEffect(() => {
+        axios.get('http://localhost:8081')
+        .then(res => {
+            setName(res.data.name);
+        })
+    }, []);
+
     return (
         <>
             <div>
-                <HealthBar />
+                <HealthBar name={name} />
                 <HealthBar2 placeholderName={username2 || 'Placeholder 2'} />
             </div>
 
--- pythonakaibu/src/components/HealthBar.jsx
+++ pythonakaibu/src/components/HealthBar.jsx
@@ -2,11 +2,12 @@ import 'beercss';
 import './HealthBar.css'
 import React from 'react'
 
-const HealthBar = ({ maxHp = 100, hp = 100 } = {}) => {
+const HealthBar = ({ maxHp = 100, hp = 100, name } = {}) => {
     const barWidth = (hp / maxHp) * 100;
+
     return (
         <div className='health-bar-container absolute left top'>
-            <span className='bar-name-1'>Placeholder 1</span>
+            <span className='bar-name-1'>{name}</span>
             <div className="health-bar">
             <div className="bar" style={{ width: `${barWidth}%` }}></div>
             <div className="hit" style={{ width: `${0}%` }}></div>
